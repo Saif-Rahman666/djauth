e04c49fbe477c1cd3f44c68627fc67503cbfc523
Saif Rahman <Saf.saif@gmail.com>
2025-06-27 23:44:18 +0600

    Settings updated

--- core/settings.py
+++ core/settings.py
@@ -147,3 +147,8 @@ ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_UNIQUE_EMAIL = True
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
