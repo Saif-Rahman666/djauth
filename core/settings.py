e2a8dfef92047b3faeb7f77779037e88c5a8f157
Saif Rahman <Saf.saif@gmail.com>
2025-07-08 00:19:06 +0600

    env updated

--- core/settings.py
+++ core/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+from os import environ as env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4)^81!w8zr9i(*_h!ux8lxcwfl%8ydi^!6oz&7&a=oob*6@#cq'
+SECRET_KEY = env["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -95,11 +96,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
-        "USER": "postgres",
-        "PASSWORD": "postgres",
-        "HOST": "localhost",
-        "PORT": "5432",
+        "NAME": env["DB_NAME"],
+        "USER": env["DB_USER"],
+        "PASSWORD": env["DB_PASSWORD"],
+        "HOST": env["DB_HOST"],
+        "PORT": env["DB_PORT"],
+        
     }
 }
 
@@ -163,9 +165,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
-EMAIL_HOST_USER = 'saf.saif@gmail.com'
-EMAIL_HOST_PASSWORD = 'excnmnfzcxwfdgmh'
-DEFAULT_FROM_EMAIL = 'saf.saif@gmail.com'
+EMAIL_HOST_USER = env["EMAIL_HOST_USER"]
+EMAIL_HOST_PASSWORD = env["EMAIL_HOST_PASSWORD"]
+DEFAULT_FROM_EMAIL = env["DEFAULT_FROM_EMAIL"]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
